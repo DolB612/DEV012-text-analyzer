0c51af4ad5f37feceda293d0921eb53420189c10
¨Sara <sara.arvizu07@gmail.com>
2023-09-26 13:49:01 -0600

    2 commit

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,21 +1,60 @@
 const analyzer = {  
   getWordCount: (text) => {
+
+    let array= text.replace(/[0-9]\s]+/g, "");
+    array=array.trim();
+    
+    if(array===""){
+      return 0;
+
+    }else{
+      array=array.split(" ");
+      return array.length;
+    }
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    
   },
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    return text.length;
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    let textWithoutSpaces = text.replace(/[^A-Za-z0-9]/g, ''); // Elimina espacios
+    textWithoutSpaces=textWithoutSpaces.replace(/\s+/g, "");
+    return textWithoutSpaces.length;
+  },
+   getNumberCount: (text) => {
+    //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    const numbers = text.match(/\b\d+(\.\d+)?\b/g); // Encuentra números y decimales
+    return numbers ? numbers.length : 0;
+  },
+  getNumbersSum: (text) => {
+     // Utilizamos una expresión regular para encontrar todos los números (enteros y decimales) en el texto
+    const numbers = text.match(/[-+]?\d*\.?\d+/g);
+
+    // Si no se encontraron números, devolvemos 0
+    if (!numbers) {
+        return 0;
+    }
+
+    // Inicializamos una variable para almacenar la suma
+    let sum = 0;
+
+    // Recorremos el array de números y los sumamos
+    for (let i = 0; i < numbers.length; i++) {
+        sum += parseFloat(numbers[i]);
+    }
+
+    return sum;
+    //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    
   },
   getAverageWordLength: (text) => {    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
-  },
-  getNumberCount: (text) => {
-    //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
-  },
-  getNumberSum: (text) => {
-    //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    const words = text.split(/\s+/).filter((word) => word !== ''); // Divide el texto en palabras
+    const totalLength = words.reduce((acc, word) => acc + word.length, 0); // Suma la longitud de las palabras
+    return words.length > 0 ? (totalLength / words.length).toFixed(2) : 0; // Calcula la longitud promedio
   },
 };
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
 import analyzer from './analyzer.js';
+// Inicializamos el contador en 0
+let Conteo = 0;
 
-//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+// TODO: Escuchar eventos del DOM e invocar los métodos del objeto `analyzer`
+document.querySelector("textarea[name='user-input']").addEventListener("input", calcularValores);
+
+function calcularValores(e) {
+  const texto = e.target.value;
+  
+  // Conteo de palabras
+  const palabras = analyzer.getWordCount(texto);
+  document.querySelector("li[data-testid='word-count']").textContent = "Palabras: " + palabras;
+
+  // Conteo de caracteres
+  const caracteres = analyzer.getCharacterCount(texto);
+  document.querySelector("li[data-testid='character-count']").textContent = "Caracteres: " + caracteres;
+
+  // Conteo de caracteres sin espacios
+  const caracteresSinEspacios = analyzer.getCharacterCountExcludingSpaces(texto);
+  document.querySelector("li[data-testid='character-no-spaces-count']").textContent = "Carácteres sin espacios: " + caracteresSinEspacios;
+
+  // Conteo de números
+  const numeros = analyzer.getNumberCount(texto);
+  document.querySelector("li[data-testid='number-count']").textContent = "Números: " + numeros;
+
+  // Suma de números
+  const sumaNumeros = analyzer.getNumbersSum(texto);
+  document.querySelector("li[data-testid='number-sum']").textContent = "Suma números: " + sumaNumeros;
+
+  // Longitud promedio de palabras
+  const longitudPromedioPalabras = analyzer.getAverageWordLength(texto);
+  document.querySelector("li[data-testid='word-length-average']").textContent = "Longitud promedio de palabras: " + longitudPromedioPalabras;
+  
+}
+
+// Limpiar botón para el textarea y los li
+let boton = document.getElementById("reset-button");
+let textarea = document.querySelector("textarea[name='user-input']");
+boton.addEventListener("click", function() {
+  textarea.value = "";
+  document.querySelector("li[data-testid='word-count']").textContent = "Palabras: 0";
+  document.querySelector("li[data-testid='character-count']").textContent = "Caracteres: 0";
+  document.querySelector("li[data-testid='character-no-spaces-count']").textContent = "Carácteres sin espacios: 0";
+  document.querySelector("li[data-testid='number-count']").textContent = "Números: 0";
+  document.querySelector("li[data-testid='number-sum']").textContent = "Suma números: 0";
+  document.querySelector("li[data-testid='word-length-average']").textContent = "Longitud promedio de palabras: 0";
+});
